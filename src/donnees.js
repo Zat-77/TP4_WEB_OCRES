3b2f8507ad82a874b6e5238fd3f48a6651d3c9f3
Kruskal <vic.delerme@gmail.com>
2020-10-26 23:00:25 +0100

    Ajout du react et de la BDD

---
+++ src/donnees.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const profils = [
+    {
+      nom: bob
+    },
+    {
+      nom: martine
+    },
+    {
+      nom: camille
+    }
+  ];
